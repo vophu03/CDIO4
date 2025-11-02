9b71fdcac3192a19eb832f11b79746e2231f826c
vophu03 <votanphu1772@gmail.com>
2025-11-02 10:21:59 +0700

    feat(ui): show ImageSlider only on home and /tours; wrap App with BrowserRouter

--- FontEnd/src/components/Layout/Layout.jsx
+++ FontEnd/src/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header/Header";
 import SideBar from "./SideBar/SideBar";
 import Footer from "./Footer/Footer";
@@ -6,10 +6,12 @@ import ImageSlider from "../Ui/ImageSlider/ImageSlider";
 import ChatBox from "../Ui/ChatBox/ChatBox";
 
 function Layout() {
+    const { pathname } = useLocation();
+    const showImageSlider = pathname === "/" || pathname === "/tours";
     return (
         <div className="min-h-screen flex flex-col w-full">
             <Header />
-            <ImageSlider />
+            {showImageSlider ? <ImageSlider /> : null}
             <div className="flex flex-1 w-full">
                 <div className="flex-1 p-4 px-20">
                     <Outlet />
